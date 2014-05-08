b3d86941570ceaacfb50a322d11319d48f31b43d
Devan <Devan@Mormon.home>
2014-05-08 11:05:41 -0700

    Addition: new class Piece

--- src/checkers/Checkers.java
+++ src/checkers/Checkers.java
@@ -15,14 +15,14 @@ import java.util.Scanner;
 public class Checkers {
     String playerOneName;
     String playerTwoName;
-    String instructions = "This is a game of checkers. You will be playing "
-            + "against the other player.\n The object of the game is to capture "
-            + "all your opponents's pieces.\n You can only move your pieces "
-            + "diagonally in the forward direction.\n You take your opponent's "
-            + "pieces by 'jumping' over them with your piece.\n If you get a "
-            + "piece to the other side of the board, you may claim a king-piece,\n "
-            + "which may move diagonally in either the forward or backwards "
-            + "direction.\n Let's play!";
+    String instructions = "This is a game of checkers. You will be playing"
+            + "against the other player.\nPeterThe object of the game is to capture"
+            + "all your opponents's pieces.\nYou can only move your pieces"
+            + "diagonally in the forward direction.\nYou take your opponent's"
+            + "pieces by 'jumping' over them with your piece.\nIf you get a"
+            + "piece to the other side of the board, you may claim a king-piece,\n"
+            + "which may move diagonally in either the forward or backwards"
+            + "direction.\nLet's play!";
     public void getName(){
         Scanner input = new Scanner(System.in);
         System.out.println("Player One, please enter name: ");
@@ -39,8 +39,10 @@ public class Checkers {
     public static void main(String[] args) {
         // TODO code application logic here
         Checkers myGame = new Checkers();
+        Piece myPiece = new Piece();
         myGame.getName();
         myGame.displayHelp();
+        myPiece.outputPieceInfo();
     }
     
 }
