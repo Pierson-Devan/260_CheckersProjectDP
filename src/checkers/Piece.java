0c10aa29a0e3cee5508ccdc2da8dfd91e0ed907f
Devan <Devan@Mormon.home>
2014-05-29 11:30:08 -0700

    change Piece class so location is two variables

--- src/checkers/Piece.java
+++ src/checkers/Piece.java
@@ -13,16 +13,14 @@ package checkers;
 public class Piece {
     String color;
     Boolean isKing = false;//marks whether a given piece is normal or a king
-    String location = null;/**marks location on the board:
-     * default is null because it is not on board. If captured,
-     * location is null. Normal location is stored as letter
-     * and number. For example, A8, B2, G1, etc.*/
+    int locationVert = 0;
+    int locationHori = 0;
     Boolean isCaptured = false;//stores if the piece is captured or not
     
     /**outputs the values in all default variables*/
     public void outputPieceInfo(){
         System.out.println("Piece is King: " + isKing +"\n"
-                + "Piece is on location: " + location + "\n"
+                + "Piece is on location: " + locationHori +" and " +locationVert + "\n"
                 + "Piece is Captured: " +isCaptured);
     }
 }
